12a070c7106d8a5c5a60e0f0f2763967419c11ff
josjual86-star <jos.jual86@gmail.com>
2026-08-04 00:19:15 -0600

    se subio la  base de datos al servidor y se intenta conectar de una vez por todas con ela, se modifico el apuntador en config.py, el SQLACHEMY linea 5

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ class Config:
 
     SECRET_KEY = "123456"
 
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://ulises:Upp2026@localhost/crud_python"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://adminupp:Claveupp.2026@127.0.0.1:3306/crud_python"
+
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
